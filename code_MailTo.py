7debede33ca411fb097aea8859c5ec24f799f4d1
Roxanne <roxanne.galura@gmail.com>
2023-04-27 12:53:04 +0200

    remplacement de la bibliothèque smtplib par imaplib

--- code_MailTo.py
+++ code_MailTo.py
@@ -1,4 +1,13 @@
-import smtplib 
+import imaplib
 
-serveur = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-serveur.login("notre_email", "mot_de_passe")
+mail = imaplib.IMAP4_SSL("imap.gmail.com")
+mail.login("roro.enzo.test@gmail.com","pnrbwptkjsvevasv") #se connecter
+
+mail.select("Inbox") #acceder aux mail qu'on reçoit
+
+clé = "FROM" #prendre comme clé FROM
+valeur = "roxanne.galura@gmail.com" # dans from prendre une adresse
+résultat, data = mail.search(None, clé, valeur) #va donc rechercher l'adresse
+
+mail_id_list = data[0].split() #obtient les id des messages et les sépare
+
