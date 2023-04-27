c0c43d9bf7cc06800447e6d1255b204829d722ab
Roxanne <roxanne.galura@gmail.com>
2023-04-27 10:36:09 +0200

    tout supp et code pour connection gmail

--- code_MailTo.py
+++ code_MailTo.py
@@ -1,19 +1,4 @@
-import requests
-import urllib.request #permettent de faire des requetes
-from bs4 import BeautifulSoup #rendre pages accessibles
-import pandas as pd #pour mettre l'objet dans un table
-from selenium import webdriver #permettra de tansformer le 
-#code dans "inspecter de la page en html"
-from time import sleep #repos 
+import smtplib 
 
-url = "https://mail.google.com/mail/u/0/?hl=fr#sent"
-css_selector = ".zA yO" #id de la ligne du tableau avec l'adresse mail
-
-wd_path = "PROJET ER/chromedriver" #chromedriver: pouvoir interagir avec la page google
-
-wdriver = webdriver.Chrome(executable_path = wd_path) 
-
-wdriver.get(url) #acceder à la page
-sleep(4) #
-
-
+serveur = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+serveur.login("notre_email", "mot_de_passe")
