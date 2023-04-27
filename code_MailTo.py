4f2c04e4b75a44456f903ad93341a9ae9ee23247
Roxanne <roxanne.galura@gmail.com>
2023-04-27 12:58:52 +0200

    fonction qui prend les données

--- code_MailTo.py
+++ code_MailTo.py
@@ -1,4 +1,5 @@
 import imaplib
+import email
 
 mail = imaplib.IMAP4_SSL("imap.gmail.com")
 mail.login("roro.enzo.test@gmail.com","pnrbwptkjsvevasv") #se connecter
@@ -11,3 +12,16 @@ résultat, data = mail.search(None, clé, valeur) #va donc rechercher l'adresse
 
 mail_id_list = data[0].split() #obtient les id des messages et les sépare
 
+messages = [] #creer liste pour mettre les infos
+for num in mail_id_list:
+    résultat, data = mail.fetch(num, "(RFC822)")
+    messages.append(data)
+
+for info in messages:
+    for response_part in info:
+        if type(response_part) is tuple:
+            mess = email.message_from_bytes(response_part[1])
+            print("subj:", mess["subject"])
+            print("from:", mess["from"])
+            print("body:")
+  
