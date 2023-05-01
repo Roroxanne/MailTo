ae73eacfc212cee2b8e14d920b0960453d3a53ac
Roxanne <roxanne.galura@gmail.com>
2023-05-01 18:26:24 +0200

    modification de la clé et valeur pour obtenir que les mails avec comme objet Contact

--- code_MailTo.py
+++ code_MailTo.py
@@ -6,8 +6,8 @@ mail.login("roro.enzo.test@gmail.com","pnrbwptkjsvevasv") #se connecter
 
 mail.select("Inbox") #acceder aux mail qu'on reçoit
 
-clé = "FROM" #prendre comme clé FROM
-valeur = "roxanne.galura@gmail.com" # dans from prendre une adresse
+clé = "SUBJECT" #prendre comme clé FROM
+valeur = "Contact" # dans from prendre une adresse
 résultat, data = mail.search(None, clé, valeur) #va donc rechercher l'adresse
 
 mail_id_list = data[0].split() #obtient les id des messages et les sépare
